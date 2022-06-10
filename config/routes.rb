d6c6cf4a13b6ac087c64e46efdb230907156f65a
n-katou <naoto.light@gmail.com>
2022-06-10 15:00:19 +0000

    管理者のビュー作成まで

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :customers
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+  }
+   namespace :admin do
+    resources :genres, only:[:index,:create,:edit,:update]
+    resources :items, only:[:index,:new,:create,:show,:edit,:update]
+    resources :customers, only:[:index,:show,:edit,:update]
+    get "/" => "homes#top", as: "home"
+    resources :orders, only:[:show,:update]
+  end
+  
+  devise_for :customers,skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
