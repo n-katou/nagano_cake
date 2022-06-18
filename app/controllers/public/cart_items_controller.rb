902eb6e38558418c0eb74dddc02650434b8c1d86
n-katou <naoto.light@gmail.com>
2022-06-18 18:14:07 +0000

    6/19 update

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -12,6 +12,15 @@ class Public::CartItemsController < ApplicationController
   end
 
   def create
+    @cart_item = Cart_item.new(cart_item_params)
+    @cart_item.save
+    redirect_to cart_items_path
+  end
+
+  private
+
+  def cart_item_params
+    params.require(:cart_item).permit(:item_id,:customer_id,:amount)
   end
 
 end
