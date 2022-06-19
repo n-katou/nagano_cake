703a5f362096348fc3cfbf5177df24fb4d40685d
n-katou <naoto.light@gmail.com>
2022-06-19 14:55:16 +0000

    カート内機能と合計機能まで

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,8 +1,14 @@
 class Public::CartItemsController < ApplicationController
   def index
+    @cart_items = current_customer.cart_items
+    @total = @cart_items.inject(0) { |sum,item| sum + item.subtotal}
+    # binding.pry
   end
 
   def update
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.update(cart_item_params)
+    redirect_to cart_items_path
   end
 
   def destroy
@@ -12,9 +18,20 @@ class Public::CartItemsController < ApplicationController
   end
 
   def create
-    @cart_item = Cart_item.new(cart_item_params)
-    @cart_item.save
-    redirect_to cart_items_path
+    @new_cart_item = CartItem.new(cart_item_params)
+    if CartItem.find_by(item_id: params[:cart_item][:item_id])
+      @cart_item = CartItem.find_by(item_id: params[:cart_item][:item_id])
+      @cart_item.amount = @cart_item.amount + @new_cart_item.amount.to_i
+      @cart_item.update(amount:@cart_item.amount)
+      redirect_to cart_items_path
+    else
+      if @new_cart_item.save
+        redirect_to cart_items_path
+      else
+       render :index
+      end
+    end
+
   end
 
   private
